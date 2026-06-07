e749847079061f5fe13d639a2555d4126646df47
Safiya <safiyashakeer98@gmail.com>
2026-06-07 13:28:48 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/petmonk/PetmonkApplication.java
@@ -0,0 +1,13 @@
+package com.petmonk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmonkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetmonkApplication.class, args);
+	}
+
+}
